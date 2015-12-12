3bee81ddcf2fb0da58c813fa4601052c4ee355f4
Holly Becker <becker.holly@gmail.com>
2015-12-11 16:34:03 -0800

    JS: base64 always assumes needs escaping

--- src/dashboard/src/media/js/vendor/base64.js
+++ src/dashboard/src/media/js/vendor/base64.js
@@ -4,19 +4,11 @@ var Base64 =
 {
     // If passed string is UTF-8, decodes to bytes first before encoding.
     encode: function (str) {
-        try {
-            return window.btoa(str);
-        } catch (InvalidCharacterError) {
-            return window.btoa(unescape(encodeURIComponent(str)));
-        }
+        return window.btoa(unescape(encodeURIComponent(str)));
     },
 
     // Returns a UTF-8 string if input is UTF-8, raw bytes otherwise.
     decode: function (str) {
-        try {
-            return decodeURIComponent(escape(window.atob(str)));
-        } catch (URIError) {
-            return window.atob(str);
-        }
+        return decodeURIComponent(escape(window.atob(str)));
     }
 };
