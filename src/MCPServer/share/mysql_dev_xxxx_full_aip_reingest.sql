3aaa16436a99b88705c31f8272e2b0d7e4623b09
Holly Becker <hbecker@artefactual.com>
2016-03-16 15:11:09 -0700

    Reingest: Parse DC & Rights metadata
    On full reingest, parse DC & PREMIS:RIGHTS metadata, but associated it with
    the soon-to-be-created SIP, since Transfer rights can't be edited in ingest.
    
    This client script can also be used to parse other external METS files, eg
    from DSpace or Dataverse.

--- src/MCPServer/share/mysql_dev_xxxx_full_aip_reingest.sql
+++ src/MCPServer/share/mysql_dev_xxxx_full_aip_reingest.sql
@@ -16,3 +16,12 @@ INSERT INTO TasksConfigs (pk, taskType, taskTypePKReference, description) VALUES
 INSERT INTO MicroServiceChainLinks(pk, microserviceGroup, defaultExitMessage, currentTask, defaultNextChainLink) values ('ae5cdd0d-2f81-4935-a380-d5c6f1337d93', 'Reject transfer', 'Failed', '1fea5138-981f-4fef-9d74-4d6328fb9248', '333532b9-b7c2-4478-9415-28a3056d58df');
 INSERT INTO MicroServiceChainLinksExitCodes (pk, microServiceChainLink, exitCode, nextMicroServiceChainLink, exitMessage) VALUES ('e8b5ea83-a108-48b4-af8e-42fa6794b1de', 'ae5cdd0d-2f81-4935-a380-d5c6f1337d93', 0, '333532b9-b7c2-4478-9415-28a3056d58df', 'Completed successfully');
 UPDATE MicroServiceChains SET startingLink='ae5cdd0d-2f81-4935-a380-d5c6f1337d93' WHERE pk='1b04ec43-055c-43b7-9543-bd03c6a778ba';
+
+-- Add parse external METS
+INSERT INTO StandardTasksConfigs (pk, requiresOutputLock, execute, arguments) VALUES ('d9708512-ac5f-4211-b07a-e5a41c6825b6', 0, 'parseExternalMETS', '%SIPUUID% "%SIPDirectory%"');
+INSERT INTO TasksConfigs (pk, taskType, taskTypePKReference, description) VALUES ('9ecf18d4-652b-4bd2-a3f5-bfb5794299f8', '36b2e239-4a57-4aa5-8ebc-7a29139baca6', 'd9708512-ac5f-4211-b07a-e5a41c6825b6', 'Parse external METS');
+INSERT INTO MicroServiceChainLinks(pk, microserviceGroup, defaultExitMessage, currentTask, defaultNextChainLink) values ('675acd22-828d-4949-adc7-1888240f5e3d', 'Complete transfer', 'Failed', '9ecf18d4-652b-4bd2-a3f5-bfb5794299f8', 'db99ab43-04d7-44ab-89ec-e09d7bbdc39d');
+INSERT INTO MicroServiceChainLinksExitCodes (pk, microServiceChainLink, exitCode, nextMicroServiceChainLink, exitMessage) VALUES ('575ab10a-560c-40ed-aa33-95a26ef52b65', '675acd22-828d-4949-adc7-1888240f5e3d', 0, 'db99ab43-04d7-44ab-89ec-e09d7bbdc39d', 'Completed successfully');
+UPDATE MicroServiceChainLinksExitCodes SET nextMicroServiceChainLink='675acd22-828d-4949-adc7-1888240f5e3d' WHERE microServiceChainLink='8ec0b0c1-79ad-4d22-abcd-8e95fcceabbc';
+UPDATE MicroServiceChainLinks SET defaultNextChainLink='675acd22-828d-4949-adc7-1888240f5e3d' WHERE pk='8ec0b0c1-79ad-4d22-abcd-8e95fcceabbc';
+UPDATE TasksConfigsUnitVariableLinkPull SET defaultMicroServiceChainLink = '675acd22-828d-4949-adc7-1888240f5e3d' WHERE pk = '49d853a9-646d-4e9f-b825-d1bcc3ba77f0';
